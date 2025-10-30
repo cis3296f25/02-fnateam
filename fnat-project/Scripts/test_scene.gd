714ff0782b4a4056e4843bcff2bd7841dcb3e730
tup80439 <tup80439@temple.edu>
2025-10-30 15:38:01 -0400

    Made my first function in godot
    feel free to delete

--- fnat-project/Scripts/test_scene.gd
+++ fnat-project/Scripts/test_scene.gd
@@ -10,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _thisIsaFunction() -> void:
+	print("Dylan Made this")
+	pass
