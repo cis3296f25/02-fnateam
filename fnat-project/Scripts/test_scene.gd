c4ca582461f99c904b388578cefe1cf67c7f4234
carlpielou03 <tul35916@temple.edu>
2025-10-30 15:10:12 -0400

    Adding my own Test Scene and Script
    This is me just adding my own test scene, so we can experiment with things on this scene before moving them somewhere else.

---
+++ fnat-project/Scripts/test_scene.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("This is coming from Carl in the Testing Scene!")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
