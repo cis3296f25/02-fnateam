38964cccb87b28e5568e16afc24818d4cec0f9e6
julescampbell <tuq28686@temple.edu>
2025-11-03 20:11:54 -0500

    mute button
    added button to mute all sounds

--- fnat-project/Scripts/options.gd
+++ fnat-project/Scripts/options.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _on_volume_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(0,linear_to_db(value))
+
+
+
+func _on_mute_check_box_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0,toggled_on)
