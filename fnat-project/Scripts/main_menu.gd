8aaa2c6c44563d036310f3293f0f86f03d512fbb
julescampbell <tuq28686@temple.edu>
2025-11-03 20:46:17 -0500

    MainMenu Quit button
    main menu quit button now sends notification to all nodes and terminates the program

--- fnat-project/Scripts/main_menu.gd
+++ fnat-project/Scripts/main_menu.gd
@@ -4,3 +4,10 @@ extends Control
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Options.tscn")
+
+
+func _on_exit_pressed() -> void:
+	# notification alerts all other nodes of the program termination
+	# we can use this to save game before closing
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
