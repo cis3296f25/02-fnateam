72fa3f5dd40a6e229cf4b1af5e1806dce64fe4be
julescampbell <tuq28686@temple.edu>
2025-11-03 20:35:51 -0500

    connected options to main menu
    main menu options button now switches to the options scene. back button goes back to main menu. this should be changed to go to whichever scene you came from if we use this options scene in the pause menu mid-game.

--- fnat-project/Scripts/options.gd
+++ fnat-project/Scripts/options.gd
@@ -7,3 +7,7 @@ func _on_volume_slider_value_changed(value):
 
 func _on_mute_check_box_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
