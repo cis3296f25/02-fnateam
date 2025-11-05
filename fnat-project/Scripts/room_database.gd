f669b4156f38918869ce7b2227960ffb5560a88e
carlpielou03 <tul35916@temple.edu>
2025-11-05 15:13:20 -0500

    Updated Room Dtabaase to Include The Scene Paths for the Camera Feed System
    !! They have an empty value and will be added in later.

--- fnat-project/Scripts/room_database.gd
+++ fnat-project/Scripts/room_database.gd
@@ -5,11 +5,13 @@ extends Node
 	1 : {
 		"Name" : "Storage", # Where Phillie FNATIC, Gritty, and Hoooter Start.
 		"AdjacentRooms" : [2],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	2 : {
 		"Name" : "Lobby", # Main HUB!!
 		"AdjacentRooms" : [3,4, 5, 6],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	3 : { #Alternate Pathing to delay the approach of Gritty
@@ -20,61 +22,73 @@ extends Node
 	4 : { # Alternate Pathing to delay the approach of Phillie Fnatic
 		"Name" : "Closet", 
 		"AdjacentRooms" : [2],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	5 : { # Connecting section between the Lobby and the Gym
 		"Name" : "Left Locker", 
 		"AdjacentRooms" : [2,7],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	6 : { # Connecting section between the Lobby and the Gym
 		"Name" : "Right Locker", 
 		"AdjacentRooms" : [2,7],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	7 : { # HUB section connecting the final defense against the mascots.
 		"Name" : "Gym", 
 		"AdjacentRooms" : [5,6,8,9,14],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	8 : { # Where the other animatronics will come down to get to the office.
 		"Name" : "Left Hallway", 
 		"AdjacentRooms" : [7,15],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	9 : { # Where the other animatronics will come down to get to the office.
 		"Name" : "Right Hallway", 
 		"AdjacentRooms" : [7,15],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	10 : { # This is where Phang will start.
 		"Name" : "Utility", 
 		"AdjacentRooms" : [2, 11],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	11 : { # Connecting section to have Phang take some time to get to the office. Connects the vents to the Utility.
 		"Name" : "Vent Section 1", 
 		"AdjacentRooms" : [10, 12],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	12 : { # Connecting section to have Phang take some time to get to the office.
 		"Name" : "Vent Section 2", 
 		"AdjacentRooms" : [11,13],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	13 : { # This will be the section linking the vents to the office. Where Phang will come throguh to get to the office.
 		"Name" : "Vent Section 3", 
 		"AdjacentRooms" : [12, 15],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	14 : { # This is where Franklin the Dog will start.
 		"Name" : "Ball Cart", 
 		"AdjacentRooms" : [7],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 	15 : { # This is where the player is going to be at. All Animatronics are trying to get to here.
 		"Name" : "Office", 
 		"AdjacentRooms" : [8,9,13],
+		"RoomPathScene" : "",
 		"SealedDoor" : false
 	},
 }
