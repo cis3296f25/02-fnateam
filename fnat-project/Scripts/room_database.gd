4720fb1357c0c619a3bb25053bd4bd1a1f555a29
carlpielou03 <tul35916@temple.edu>
2025-11-04 23:10:10 -0500

    Added Some Testing of The Room Database
    JUst added room databse scene to the test scene.

--- fnat-project/Scripts/room_database.gd
+++ fnat-project/Scripts/room_database.gd
@@ -1,6 +1,14 @@
 extends Node
 
 
+@export var rooms = {
+	1 : {
+		"Name" : "TestRoom1", 
+		"AdjacentRooms" : {}
+		
+	}
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- fnat-project/Scripts/test_scene.gd
+++ fnat-project/Scripts/test_scene.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("This is coming from Carl in the Testing Scene!")
+	print($RoomDatabase.rooms)
 	julesTestFunc()
 	pass # Replace with function body.
 
