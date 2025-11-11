42bac34755f7af08def612e71e213358d4cf8362
tup80439 <tup80439@temple.edu>
2025-11-11 00:25:18 -0500

    flash test

---
+++ fnat-project/Scripts/FlashLight.gd
@@ -0,0 +1,40 @@
+extends Node
+var counter = 0
+
+
+var Flash = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		Flash = true
+		print("Flashlight on")
+	else:
+		Flash = false
+		print("Flashlight off")
+		
+func _on_button_pressed() -> void:
+	if counter % 2 == 0:
+		_on_button_toggled(1)
+		scareAway()
+	else:
+		_on_button_toggled(0)
+	counter += 1
+	pass # Replace with function body.
+
+func scareAway() -> void:
+	var mascots := ["Phang", "Phillies Fnatic", "Hooter", "Gritty"]
+	
+	for i in range(4):
+		if GameManager.animatronics_locations[mascots[i]] == "Office" && Flash:
+			GameManager.animatronic_flashed.connect(mascots[i])
+		
+		
