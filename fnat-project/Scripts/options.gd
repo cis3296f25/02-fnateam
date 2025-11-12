3e4e2fe82d255e6e413cc0696d654dc27c9e79e1
julescampbell <tuq28686@temple.edu>
2025-11-12 14:39:27 -0500

    toggle fullscreen from options

--- fnat-project/Scripts/options.gd
+++ fnat-project/Scripts/options.gd
@@ -11,3 +11,16 @@ func _on_mute_check_box_toggled(toggled_on: bool) -> void:
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+
+@onready var previous_window = DisplayServer.window_get_mode()
+@onready var current_window = DisplayServer.window_get_mode()
+func _on_fullscreen_toggle_pressed() -> void:
+	current_window = DisplayServer.window_get_mode()
+	if current_window != 4:
+		previous_window = current_window
+		DisplayServer.window_set_mode(4)
+	else:
+		if previous_window == 4:
+			previous_window = 2
+		DisplayServer.window_set_mode(previous_window)
