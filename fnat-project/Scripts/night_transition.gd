ff7b025daf8e6eb6a58bf8b5b8698566f7f3765c
Han Wang <150043566+hanwang001@users.noreply.github.com>
2025-11-18 16:26:42 -0500

    Implement Save Game progress system

--- fnat-project/Scripts/night_transition.gd
+++ fnat-project/Scripts/night_transition.gd
@@ -2,8 +2,9 @@ extends Control
 
 @onready var night_label = $Panel/Night
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Night_Transition: current_night =", GameManager.current_night)
+
 	match GameManager.current_night:
 		1:
 			night_label.text = "1st Night"			
@@ -12,13 +13,8 @@ func _ready() -> void:
 		3:
 			night_label.text = "3rd Night"
 		_:
-			night_label.text =  str(GameManager.current_night) + "th Night"
+			night_label.text = str(GameManager.current_night) + "th Night"
 			print("We don't know what Night we are in.")
+
 	await get_tree().create_timer(2.5).timeout
 	get_tree().change_scene_to_file("res://Scenes/SecurityDesk.tscn")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
