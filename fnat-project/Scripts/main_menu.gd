4a72cc4b4b436ead22e801e0377ef1ac53b6e87f
Han Wang <150043566+hanwang001@users.noreply.github.com>
2025-11-20 14:31:47 -0500

    Add reset save data button and confirmation dialog

--- fnat-project/Scripts/main_menu.gd
+++ fnat-project/Scripts/main_menu.gd
@@ -5,6 +5,9 @@ func _ready() -> void:
 	print("Main menu ready. has_save =", SaveManager.has_save)
 
 
+# ------------------------
+# Start Game
+# ------------------------
 func _on_start_pressed() -> void:
 	print(">>> Start pressed")
 
@@ -14,6 +17,10 @@ func _on_start_pressed() -> void:
 
 	get_tree().change_scene_to_file("res://Scenes/Night_Transition.tscn")
 
+
+# ------------------------
+# Continue Game
+# ------------------------
 func _on_Continue_pressed() -> void:
 	print(">>> Continue pressed")
 
@@ -23,10 +30,37 @@ func _on_Continue_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Night_Transition.tscn")
 
 
+# ------------------------
+# Options
+# ------------------------
 func _on_Options_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Options.tscn")
 
 
+# ------------------------
+# Exit Game
+# ------------------------
 func _on_Exit_pressed() -> void:
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 	get_tree().quit()
+
+
+
+# ------------------------
+func _on_reset_pressed() -> void:
+	print(">>> Reset pressed")
+	$Panel/ResetDialog.popup_centered()
+
+
+# ------------------------
+# Reset Dialog Confirmed
+# ------------------------
+func _on_reset_dialog_confirmed() -> void:
+	print(">>> Reset Confirmed")
+
+	SaveManager.reset_save()
+	GameManager.current_night = 1
+
+	$Container/Continue.disabled = true
+
+	print("All save data cleared.")
