a3371ed6a8a3b065ce6a008dd51646630b5f3b14
carlpielou03 <tul35916@temple.edu>
2025-11-21 02:04:53 -0500

    Balanced Mascots!!

--- fnat-project/Scripts/night_database.gd
+++ fnat-project/Scripts/night_database.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var Night = {
-	1 : {
+	1 : { # Active Mascots, FNATIC, GRITTY, and HOOTER
 		"PhangAI_Start" : 0,
 		"PhangAI_2AMIncr" : 0,
 		"PhangAI_3AMIncr" : 0,
@@ -10,142 +10,142 @@ extends Node
 		"PhanaticAI_Start" : 0,
 		"PhanaticAI_2AMIncr" : 1,
 		"PhanaticAI_3AMIncr" : 2,
-		"PhanaticAI_4AMIncr" : 3,
+		"PhanaticAI_4AMIncr" : 1,
 
 		"GrittyAI_Start" : 0,
 		"GrittyAI_2AMIncr" : 0,
 		"GrittyAI_3AMIncr" : 1,
-		"GrittyAI_4AMIncr" : 2,
+		"GrittyAI_4AMIncr" : 1,
 
 		"HooterAI_Start" : 0,
-		"HooterAI_2AMIncr" : 0,
+		"HooterAI_2AMIncr" : 1,
 		"HooterAI_3AMIncr" : 1,
-		"HooterAI_4AMIncr" : 2,
+		"HooterAI_4AMIncr" : 1,
 
 		"FranklinAI_Start" : 0,
-		"FranklinAI_2AMIncr" : 1,
-		"FranklinAI_3AMIncr" : 1,
-		"FranklinAI_4AMIncr" : 2,
+		"FranklinAI_2AMIncr" : 0,
+		"FranklinAI_3AMIncr" : 0,
+		"FranklinAI_4AMIncr" : 0,
 
 		"PassiveDrain" : 0,
 		"MaxDrainCountdown" : 6
 	},
 
-	2 : {
+	2 : { # We will introduce Phang and Franklin.
 		"PhangAI_Start" : 2,
 		"PhangAI_2AMIncr" : 0,
-		"PhangAI_3AMIncr" : 0,
+		"PhangAI_3AMIncr" : 3,
 		"PhangAI_4AMIncr" : 0,
 
-		"PhanaticAI_Start" : 1,
+		"PhanaticAI_Start" : 5,
 		"PhanaticAI_2AMIncr" : 1,
 		"PhanaticAI_3AMIncr" : 2,
-		"PhanaticAI_4AMIncr" : 3,
+		"PhanaticAI_4AMIncr" : 1,
 
-		"GrittyAI_Start" : 1,
-		"GrittyAI_2AMIncr" : 0,
-		"GrittyAI_3AMIncr" : 1,
-		"GrittyAI_4AMIncr" : 2,
+		"GrittyAI_Start" : 5,
+		"GrittyAI_2AMIncr" : 1,
+		"GrittyAI_3AMIncr" : 2,
+		"GrittyAI_4AMIncr" : 1,
 
-		"HooterAI_Start" : 1,
+		"HooterAI_Start" : 2,
 		"HooterAI_2AMIncr" : 0,
-		"HooterAI_3AMIncr" : 1,
-		"HooterAI_4AMIncr" : 2,
+		"HooterAI_3AMIncr" : 2,
+		"HooterAI_4AMIncr" : 1,
 
-		"FranklinAI_Start" : 1,
+		"FranklinAI_Start" : 2,
 		"FranklinAI_2AMIncr" : 1,
-		"FranklinAI_3AMIncr" : 1,
-		"FranklinAI_4AMIncr" : 2,
+		"FranklinAI_3AMIncr" : 2,
+		"FranklinAI_4AMIncr" : 0,
 
 		"PassiveDrain" : 0,
 		"MaxDrainCountdown" : 6
 	},
 
 	3 : {
-		"PhangAI_Start" : 5,
+		"PhangAI_Start" : 10,
 		"PhangAI_2AMIncr" : 0,
 		"PhangAI_3AMIncr" : 0,
 		"PhangAI_4AMIncr" : 0,
 
-		"PhanaticAI_Start" : 6,
-		"PhanaticAI_2AMIncr" : 1,
-		"PhanaticAI_3AMIncr" : 2,
-		"PhanaticAI_4AMIncr" : 3,
+		"PhanaticAI_Start" : 7,
+		"PhanaticAI_2AMIncr" : 0,
+		"PhanaticAI_3AMIncr" : 3,
+		"PhanaticAI_4AMIncr" : 0,
 
-		"GrittyAI_Start" : 4,
+		"GrittyAI_Start" : 7,
 		"GrittyAI_2AMIncr" : 0,
-		"GrittyAI_3AMIncr" : 1,
-		"GrittyAI_4AMIncr" : 2,
+		"GrittyAI_3AMIncr" : 3,
+		"GrittyAI_4AMIncr" : 0,
 
-		"HooterAI_Start" : 4,
+		"HooterAI_Start" : 3,
 		"HooterAI_2AMIncr" : 0,
-		"HooterAI_3AMIncr" : 1,
+		"HooterAI_3AMIncr" : 0,
 		"HooterAI_4AMIncr" : 2,
 
-		"FranklinAI_Start" : 4,
-		"FranklinAI_2AMIncr" : 1,
-		"FranklinAI_3AMIncr" : 1,
-		"FranklinAI_4AMIncr" : 2,
+		"FranklinAI_Start" : 3,
+		"FranklinAI_2AMIncr" : 0,
+		"FranklinAI_3AMIncr" : 0,
+		"FranklinAI_4AMIncr" : 4,
 
 		"PassiveDrain" : 0,
 		"MaxDrainCountdown" : 6
 	},
 
 	4 : {
-		"PhangAI_Start" : 8,
+		"PhangAI_Start" : 5,
 		"PhangAI_2AMIncr" : 0,
-		"PhangAI_3AMIncr" : 0,
+		"PhangAI_3AMIncr" : 5,
 		"PhangAI_4AMIncr" : 0,
 
 		"PhanaticAI_Start" : 10,
-		"PhanaticAI_2AMIncr" : 1,
+		"PhanaticAI_2AMIncr" : 0,
 		"PhanaticAI_3AMIncr" : 2,
-		"PhanaticAI_4AMIncr" : 3,
+		"PhanaticAI_4AMIncr" : 1,
 
-		"GrittyAI_Start" : 6,
+		"GrittyAI_Start" : 10,
 		"GrittyAI_2AMIncr" : 0,
-		"GrittyAI_3AMIncr" : 1,
-		"GrittyAI_4AMIncr" : 2,
+		"GrittyAI_3AMIncr" : 2,
+		"GrittyAI_4AMIncr" : 1,
 
-		"HooterAI_Start" : 7,
-		"HooterAI_2AMIncr" : 0,
-		"HooterAI_3AMIncr" : 1,
+		"HooterAI_Start" : 4,
+		"HooterAI_2AMIncr" : 1,
+		"HooterAI_3AMIncr" : 3,
 		"HooterAI_4AMIncr" : 2,
 
-		"FranklinAI_Start" : 6,
+		"FranklinAI_Start" : 4,
 		"FranklinAI_2AMIncr" : 1,
 		"FranklinAI_3AMIncr" : 1,
-		"FranklinAI_4AMIncr" : 2,
+		"FranklinAI_4AMIncr" : 1,
 
 		"PassiveDrain" : 0,
 		"MaxDrainCountdown" : 6
 	},
 
 	5 : {
-		"PhangAI_Start" : 9,
+		"PhangAI_Start" : 5,
 		"PhangAI_2AMIncr" : 0,
-		"PhangAI_3AMIncr" : 0,
-		"PhangAI_4AMIncr" : 0,
+		"PhangAI_3AMIncr" : 1,
+		"PhangAI_4AMIncr" : 1,
 
-		"PhanaticAI_Start" : 10,
+		"PhanaticAI_Start" : 12,
 		"PhanaticAI_2AMIncr" : 1,
-		"PhanaticAI_3AMIncr" : 2,
-		"PhanaticAI_4AMIncr" : 3,
+		"PhanaticAI_3AMIncr" : 1,
+		"PhanaticAI_4AMIncr" : 2,
 
 		"GrittyAI_Start" : 12,
-		"GrittyAI_2AMIncr" : 0,
+		"GrittyAI_2AMIncr" : 1,
 		"GrittyAI_3AMIncr" : 1,
 		"GrittyAI_4AMIncr" : 2,
 
 		"HooterAI_Start" : 10,
 		"HooterAI_2AMIncr" : 0,
-		"HooterAI_3AMIncr" : 1,
-		"HooterAI_4AMIncr" : 2,
+		"HooterAI_3AMIncr" : 2,
+		"HooterAI_4AMIncr" : 0,
 
 		"FranklinAI_Start" : 10,
-		"FranklinAI_2AMIncr" : 1,
+		"FranklinAI_2AMIncr" : 0,
 		"FranklinAI_3AMIncr" : 1,
-		"FranklinAI_4AMIncr" : 2,
+		"FranklinAI_4AMIncr" : 1,
 
 		"PassiveDrain" : 0,
 		"MaxDrainCountdown" : 6
