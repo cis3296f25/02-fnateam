c5ca7dd608a5a5a8bc5a694a8ecc015f6af293fd
carlpielou03 <tul35916@temple.edu>
2025-12-04 18:47:06 -0500

    Added More Phone Calls to each night!

--- fnat-project/Scripts/phone_call.gd
+++ fnat-project/Scripts/phone_call.gd
@@ -3,6 +3,10 @@ extends Node2D
 @onready var phone_ring = $PhoneRinging
 @onready var phone_transition = $Transition
 @onready var night_one = $NightOnePhoneCall
+@onready var night_two = $NightTwoPhoneCall
+@onready var night_three = $NightThreePhoneCall
+@onready var night_four = $NightFourPhoneCall
+@onready var night_five = $NightFivePhoneCall
 
 func play_Ring():
 	phone_ring.play()
@@ -16,7 +20,18 @@ func _ready() -> void:
 		1:
 			await play_Ring()
 			night_one.play()
-			pass
+		2:
+			await play_Ring()
+			night_two.play()
+		3:
+			await play_Ring()
+			night_three.play()
+		4:
+			await play_Ring()
+			night_four.play()
+		5:
+			await play_Ring()
+			night_five.play()
 	pass # Replace with function body.
 
 
